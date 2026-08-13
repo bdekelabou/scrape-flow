8f95619e0462e2098c24e91ea8bb9e0dc00d06b1
ange <121102742+bdekelabou@users.noreply.github.com>
2026-08-13 09:16:03 +0000

    feat: set up authentication

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,18 @@
 'use client';
 
-import { CoinsIcon, HomeIcon, Icon, Layers2Icon, ShieldCheckIcon } from 'lucide-react';
-import React from 'react';
+import { CoinsIcon, HomeIcon, Layers2Icon, MenuIcon, ShieldCheckIcon } from 'lucide-react';
+import Logo from '@/components/logo';
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { Button, buttonVariants } from './ui/button';
+import { usePathname } from 'next/navigation';
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
 
 const routes = [
     {
@@ -27,14 +38,84 @@ const routes = [
 ];
 
 function DesktopSidebar() {
+    const pathname = usePathname();
+
+    const activeRoute = 
+        routes.find(
+            (route) => route.href.length > 0 && pathname.includes(route.href)
+        ) || routes[0];
+
   return (
-    <div className='hidden relative md:block min-w-[280px] h-screen 
-    overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 
-    dark:text-foreground text-muted-foreground border-r-2 border-separate'>
-        DesktopSidebar
+    <div className='hidden relative md:block min-w-[280px] 
+    max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 
+    dark:bg-secondary/30 dark:text-foreground text-muted-foreground border-r-2 border-separate'>
+        <div className='flex items-center justify-center gap-2 border-b-[1px] border-separate p-4'>
+            <Logo />
+        </div>
+        <div className='p-2'> TODO CREDITS</div>
+        <div className='flex flex-col p-2'>
+            {routes.map((route) => (
+                <Link 
+                    key={route.href}
+                    href={route.href}
+                    className={buttonVariants({
+                        variant: activeRoute.href === route.href ? 'sidebarActiveItem' : 'sidebarItem',
+                    })} 
+                >
+                <route.Icon size={20} />
+                    {route.label} 
+                </Link> 
+            ))}
+        </div>
     </div>
     );
   
+}
+
+export function MobileSidebar() {
+    const [isOpen, setOpen] = useState(false);
+    const pathname = usePathname();
+    
+    const activeRoute = 
+        routes.find(
+            (route) => route.href.length > 0 && pathname.includes(route.href)
+        ) || routes[0];
+
+
+    return (
+        <div className='block border-separate bg-background md:hidden'>
+             <nav className='container flex items-center justify-between px-8'>
+                <Sheet open={isOpen} onOpenChange={setOpen}>
+                    <SheetTrigger asChild>
+                    <Button variant={'ghost'} size={'icon'} >
+                        <MenuIcon />
+                    </Button>
+                    </SheetTrigger> 
+                    <SheetContent 
+                        className='w-[400px] sm:w-[540px] space-y-4'
+                        side={'left'}
+                    >
+                        <Logo />
+                        <div className='flex flex-col gap-1'>
+                            {routes.map((route) => (
+                                <Link 
+                                    key={route.href}
+                                    href={route.href}
+                                    className={buttonVariants({
+                                        variant: activeRoute.href === route.href ? 'sidebarActiveItem' : 'sidebarItem',
+                                        })} 
+                                    onClick={() => setOpen((prev) => !prev)}
+                                >
+                                    <route.Icon size={20} />
+                                        {route.label} 
+                                </Link> 
+                            ))}
+                        </div>
+                    </SheetContent>
+                </Sheet>
+             </nav>
+        </div>
+    );
 }
 
 export default DesktopSidebar;
